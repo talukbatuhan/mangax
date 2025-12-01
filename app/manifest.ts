4e5a004b8a3ac00291e088d8ab61a6360b44f560
Batuhan Taluk <wtaluk2002@gmail.com>
2025-12-01 09:28:44 +0300

    Green Book Ekledim uwu

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0a0a0a',
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/GreenBook192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512.png',
+        src: '/GreenBook512.png',
         sizes: '512x512',
         type: 'image/png',
       },
